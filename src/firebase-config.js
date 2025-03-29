d92e7ad4497c3bb1121642bd0917690d8c859dfb
Yashasvi49 <ssuhani387@gmail.com>
2025-03-29 23:22:24 +0530

    initiate structure and code

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,15 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGLLSlFVSnIgXfD-AXWVk8d6rFFBnreR8",
-  authDomain: "blogbyte-d53e5.firebaseapp.com",
-  projectId: "blogbyte-d53e5",
-  storageBucket: "blogbyte-d53e5.appspot.com",
-  messagingSenderId: "995859078377",
-  appId: "1:995859078377:web:d33fabd26bbd839f8ce478",
-  measurementId: "G-FYFX4KBGSH"
+  apiKey: "AIzaSyBJ6Y56S-myll1iZpKCXG4fJvP7_KckNEg",
+  authDomain: "easytype-9ac7e.firebaseapp.com",
+  projectId: "easytype-9ac7e",
+  storageBucket: "easytype-9ac7e.firebasestorage.app",
+  messagingSenderId: "105241435574",
+  appId: "1:105241435574:web:748e89f969ba2375e47517",
+  measurementId: "G-GQLHC95LH2"
 };
-
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
